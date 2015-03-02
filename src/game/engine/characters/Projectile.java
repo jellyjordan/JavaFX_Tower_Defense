862968e49b679237f1bdcdf062365090b0df6de7
Jordan La Croix <jordanlacroix@gmail.com>
2015-03-01 21:42:52 -0500

    Constructor now takes monster instead of their coordinates to improve animation.

--- src/game/engine/characters/Projectile.java
+++ src/game/engine/characters/Projectile.java
@@ -3,31 +3,33 @@ package game.engine.characters;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
-/*
-    Projectile is created when the tower attacks a monster. The GameManager
-    will create the animation using the start and end locations.
-*/
+/**
+ * Projectile is created when the tower attacks a monster. The GameManager
+ * will create the animation using the start and end locations.
+ */
 public class Projectile extends Circle {
-
-    private final int startX;    // Starting location of the projectile
+    private Monster target;     // The target of the attack
+    private final int startX;   // Starting location of the projectile
     private final int startY;
-    private final int endX;         // Destination of projectile
-    private final int endY;
 
-    Projectile(int towerX , int towerY ,int monsterX , int monsterY , Color color){
+
+    Projectile(Monster target , int towerX , int towerY , Color color){
         super(towerX , towerY , 5 , color);
+        this.target = target;
         startX = towerX;
         startY = towerY;
-        endX = monsterX;
-        endY = monsterY;
+    }
+
+    public Monster getTarget(){
+        return target;
     }
 
     public int getEndX(){
-        return endX;
+        return target.getX();
     }
 
     public int getEndY(){
-        return endY;
+        return target.getY();
     }
 
     public int getStartX(){
